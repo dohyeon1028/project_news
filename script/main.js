f8469412f226ac68ee0fad40515436ecaabbf2eb
dohyeon1028 <kdh051028a@gmail.com>
2023-08-22 00:54:26 +0900

    new틀 완성

---
+++ script/main.js
@@ -0,0 +1,8 @@
+const main_news = document.querySelector("#main_news");
+const article_width = document.getElementById("width").
+clientWidth;
+console.log(article_width);
+const articles = document.querySelectorAll("article");
+console.log(articles);
+
+
